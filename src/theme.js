3fbed8659495dff1c63ffcf0725649e631011439
Trung Nguyen <trung.nguyen@msn.com>
2024-01-28 15:43:11 +0700

    Board content

--- src/theme.js
+++ src/theme.js
@@ -1,8 +1,14 @@
 import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
+
+const APP_BAR_HEIGHT = '58px'
+const BOARD_BAR_HEIGHT = '58px'
+const BOARD_CONTENT_HEIGHT = `calc(100vh - ${APP_BAR_HEIGHT} - ${BOARD_BAR_HEIGHT})`
+
 const theme = extendTheme({
   trello: {
-    appBarHeight: '58px',
-    boardBarHeight: '58px'
+    appBarHeight: APP_BAR_HEIGHT,
+    boardBarHeight: BOARD_BAR_HEIGHT,
+    boardContentHeight: BOARD_CONTENT_HEIGHT
   },
   colorSchemes: {
     light: {},
@@ -16,13 +22,12 @@ const theme = extendTheme({
             width: '8px',
             height: '8px'
           },
+
           '*::-webkit-scrollbar-thumb': {
             backgroundColor: '#dcdde1',
             borderRadius: '8px'
           },
-          '*::-webkit-scrollbar-thumb:hover': {
-            backgroundColor: '#ffffff'
-          }
+          '*::-webkit-scrollbar-thumb:hover': { backgroundColor: '#fff' }
         }
       }
     },
@@ -39,6 +44,13 @@ const theme = extendTheme({
       styleOverrides: {
         root: {
           fontSize: '0.875rem'
+        }
+      }
+    },
+    MuiTypography: {
+      styleOverrides: {
+        root: {
+          '&.MuiTypography-body1': { fontSize: '0.875rem' }
         }
       }
     },
